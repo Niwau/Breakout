c6e651e6fa03826f56f613ec76c22e79621efcb3
Guilherme <xxguilhermesoaresxx@gmail.com>
2023-07-16 18:49:03 -0300

    feat: sounds and trails

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,15 +8,22 @@ public class Ball : MonoBehaviour
     public float speed = 0.1f;
     private Vector2 direction = new Vector2(1,-1);
     private Rigidbody2D rb;
-
+    private TrailRenderer trail;
+    private AudioSource audioSource;
     void Start() {
+        trail = GetComponent<TrailRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
         rb.AddForce(direction * speed);
     }
 
     void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("block"))
         {
+            audioSource.Play();
+            Color blockColor = other.gameObject.GetComponent<SpriteRenderer>().color;
+            trail.startColor = blockColor;
+            this.gameObject.GetComponent<SpriteRenderer>().color = blockColor;
             Destroy(other.gameObject);
         }
     }
